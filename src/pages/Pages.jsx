f09fc116c5a230608d18806e6f6886c072c3f769
llvovll89 <svvvs5579@naver.com>
2023-02-04 22:16:49 +0900

    react-router-dom 주석처리

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './Home';
-import { Recode } from './Recode';
-import { Code } from './Code';
+// import { Recode } from './Recode';
+// import { Code } from './Code';
 
 export const Pages = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-            basename={process.env.PUBLIC_URL}
-          />
-          <Route path="/record" element={<Recode />} />
-          <Route path="/code" element={<Code />} />
-        </Routes>
-      </Router>
+    <Home />
     </>
   );
 };
